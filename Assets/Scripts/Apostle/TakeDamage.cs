f57b5b2fe4e964fcc35863afed0f26a65e7e8d0c
marleymoore <62036010+marleymoore@users.noreply.github.com>
2026-01-14 17:09:25 +0000

    Hit Chance
    Added hit chance to moves usimg move accuracy

--- Assets/Scripts/Apostle/TakeDamage.cs
+++ Assets/Scripts/Apostle/TakeDamage.cs
@@ -1,21 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class TakeDamage : EventData
 {
     public TakeDamage(Move move, Apostle attacker, Apostle defender)
     {
-        
-        float a = (2 * attacker.Level + 10) / 250f;
-        float b = a * move.Base.Power * ((float)attacker.Attack / defender.Defence) + 2;
-        int damage = Mathf.FloorToInt(b);
-
-        defender.CurrentHP -= damage;
-        if (defender.CurrentHP <= 0)
+        int miss = UnityEngine.Random.Range(0, 100);
+        /* crit chance, hit chance */
+        if(move.Base.Accuracy >= miss)
         {
-            defender.CurrentHP = 0;
-            
+            float a = (2 * attacker.Level + 10) / 250f;
+            float b = a * move.Base.Power * ((float)attacker.Attack / defender.Defence) + 2;
+            int damage = Mathf.FloorToInt(b);
+
+            defender.CurrentHP -= damage;
+            if (defender.CurrentHP <= 0)
+            {
+                defender.CurrentHP = 0;
+
+            }
+        }
+        else
+        {
+            Debug.Log("missed");
         }
 
          
